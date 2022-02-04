1466e4faa19896e171fec29fdf0a69b9bdf03a49
oscar mureti <oscarmureti75@gmail.Com>
2022-02-04 15:00:58 +0300

    first commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+enum CounterAction {
+  Increment,
+  Decrement,
+  Reset,
+}
+
+class CounterBlock {
+  int counter = 0;
+//stream controlers
+  final _stateStreamController = StreamController<int>();
+  StreamSink<int> get CounterSink => _stateStreamController.sink;
+  Stream<int> get CounterStream => _stateStreamController.stream;
+
+//event stream controlers
+  final _eventStreamController = StreamController<CounterAction>();
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
+
+  // creating a constructor
+  CounterBlock() {
+    eventStream.listen((event) {
+      if (event == CounterAction.Increment) {
+        counter++;
+      } else if (event == CounterAction.Decrement) {
+        if (counter > 0) {
+          counter--;
+        } else {
+          counter = 0;
+        }
+      } else {
+        counter = 0;
+      }
+      CounterSink.add(counter);
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:test/counter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final counterBloc = CounterBlock();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder(
+                stream: counterBloc.CounterStream,
+                initialData: 0,
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.data.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                counterBloc.eventSink.add(CounterAction.Increment);
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                counterBloc.eventSink.add(CounterAction.Decrement);
+              },
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                counterBloc.eventSink.add(CounterAction.Reset);
+              },
+              tooltip: 'reset',
+              child: const Icon(Icons.loop),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
